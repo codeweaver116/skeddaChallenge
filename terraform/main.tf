247d1f5f06c18bb41a4f5f536198f03085f47878
Matsumu Monday <mmatsumu19@gmail.com>
2023-02-21 13:23:38 -0800

    swapped to another azure account

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,26 +9,30 @@ terraform {
     }
   }
    backend "azurerm" {
-        resource_group_name  = "mm"
-        storage_account_name = "terraformstate2343"
+        resource_group_name  = "terraform"
+        storage_account_name = "terraformstateskedda"
         container_name       = "tfstate"
         key                  = "terraform.tfstate"
     }
+
   required_version = ">= 0.14.9"
+
 }
 provider "azurerm" {
+
   skip_provider_registration = "true"
   features {}
+
+  subscription_id   = "b29e0319-fc3e-4a92-a329-2f42a6b55708"  #Bad practice, this is for the purpose of testing and demo.
+  tenant_id         = "8af79369-0252-4816-893a-d143b54885e3"
+  client_id         = "e86f1bfa-58b0-47f2-9dac-6f61de199e08"
+  client_secret     = "gw88Q~N1Udw_fEEkpneMtwb3idoZhkK2bnywWbMP"
+
+  
 }
 
 
-data "azurerm_resource_group" "skedda_resource" {
-  name = var.resouce_group
-}
-
-data "azurerm_service_plan" "skedda_app_service_plan" {
-
-  name                = var.skedda_app_service_plan
-  resource_group_name = data.azurerm_resource_group.skedda_resource.name
-
+resource "azurerm_resource_group" "skedda_resource_group" {
+  name     = var.resouce_group
+  location = var.resource_location
 }
