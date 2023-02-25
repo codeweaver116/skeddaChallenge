4c66126b9c809d819b0e2b37ab77e45a565c1824
Matsumu Monday <mmatsumu19@gmail.com>
2023-02-25 01:39:42 -0800

    added terraform backend configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "terraform"
     storage_account_name = "terraformstateskedda"
-    container_name       = "tfstate"
+    container_name       = "tfstatedr"
     key                  = "terraform.tfstate"
   }
 
